fd4804dfff98a1cdf327bd02f32ab705ecca8867
Scott <scottgangemi@gmail.com>
2016-06-07 20:42:50 +1000

    Updating to work better

--- index.js
+++ index.js
@@ -13,8 +13,9 @@ var input = '';
 var app = express();
 //app.use(express.static(__dirname)); // Current directory is root
 app.use(express.static(path.join(__dirname, 'public'))); 
-app.listen(3000, function() {
-});
+var port = process.env.PORT || 8080;
+app.listen(port);
+
 
 // app.get('/', function(req, res) {
 //     //res.sendFile('index.html', {'root': '../'});
